f6d224273b6ab782ceda18663f882bca4e8416d6
Ed Downs <ed@labradoratory.com>
2021-03-08 16:27:12 -0800

    Removes actions in exchange for extensions that do the same thing.

--- src/Labradoratory.Fetch.AddOn.Pagination/PaginationRepositoryExtensions.cs
+++ src/Labradoratory.Fetch.AddOn.Pagination/PaginationRepositoryExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Labradoratory.Fetch.AddOn.Pagination
 {
@@ -34,6 +36,33 @@ namespace Labradoratory.Fetch.AddOn.Pagination
         {
             if (repository is ISupportsPagination<TEntity> sp)
                 return sp.GetPageAsync(page, pageSize, cancellationToken);
+
+            throw RepositoryNotExtended<TEntity>();
+        }
+
+        /// <summary>
+        /// Gets the page of entities.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="repository">The repository.</param>
+        /// <param name="page">The page number.</param>
+        /// <param name="pageSize">Size of the page.</param>
+        /// <param name="cancellationToken">[Optional] The token to monitor for cancellation requests.</param>
+        /// <returns>The page of entities.</returns>
+        public static Task<ResultPage<TEntity>> GetPageAsync<TEntity>(this Repository<TEntity> repository, HttpRequest httpRequest, CancellationToken cancellationToken = default) where TEntity : Entity, IPageable
+        {
+            if (repository is ISupportsPagination<TEntity> sp)
+            {
+                var page = 0;
+                if (httpRequest.Query.TryGetValue("page", out var values) && int.TryParse(values.First(), out var value))
+                    page = value;
+
+                var pageSize = 100;
+                if (httpRequest.Query.TryGetValue("pagesize", out values) && int.TryParse(values.First(), out value))
+                    pageSize = value;
+
+                return sp.GetPageAsync(page, pageSize, cancellationToken);
+            }
 
             throw RepositoryNotExtended<TEntity>();
         }
